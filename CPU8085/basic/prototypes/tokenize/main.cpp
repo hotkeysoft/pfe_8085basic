8773c9a9fa60b971a0a64159bc4df6388568c6fa
hotkey <dev@hotkeysoft.net>
2001-08-23 20:35:56 +0000

    Test floats added tokenize2 to test

--- CPU8085/basic/prototypes/tokenize/main.cpp
+++ CPU8085/basic/prototypes/tokenize/main.cpp
@@ -10,12 +10,15 @@ int main(int argc, char* argv[])
 {
 	char str[256];
 	char tok1[256];
+	char tok2[256];
 
-	strcpy(str, "10 let variable$ = \"blah\" + chr$(147): i = sqr ( abs ( -2.5 * B - -Z) )");
+	strcpy(str, "10 a = -123.45E+2: let variable$ = \"blah\" + chr$(147): i = sqr ( abs ( -2.5 * B - -Z) )");
 
 	tokenize1(str, tok1);
+	tokenize2(tok1, tok2);
 
-	std::cout << untokenize(tok1) << std::endl;
+	std::cout << untokenize(tok1) << std::endl << std::endl;
+	std::cout << untokenize(tok2) << std::endl;
 	
 
 	return 0;
