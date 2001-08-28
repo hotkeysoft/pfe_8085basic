10b9a1e1a35315e3bca18d39e6873c8b18df7eab
hotkey <dev@hotkeysoft.net>
2001-08-28 02:35:07 +0000

    Implemented 'loop' to facilitate tests

--- CPU8085/basic/prototypes/tokenize/main.cpp
+++ CPU8085/basic/prototypes/tokenize/main.cpp
@@ -5,6 +5,8 @@
 
 #include "tokenize.h"
 #include "untokenize.h"
+#include <iostream>
+#include <string>
 
 int main(int argc, char* argv[])
 {
@@ -12,20 +14,23 @@ int main(int argc, char* argv[])
 	char tok1[256];
 	char tok2[256];
 
-	strcpy(str, "10 a = -123.45E+2: let variable$ = \"blah\" + chr$(147): i = sqr ( abs ( -2.5 * B - -Z) )");
+	std::string line;
 
-	try
+	while (1)
 	{
-		tokenize1(str, tok1);
-		tokenize2(tok1, tok2);
+		std::cin.getline(str, 255);
 
-		std::cout << str << std::endl << std::endl;
-		std::cout << untokenize(tok1) << std::endl << std::endl;
-		std::cout << untokenize(tok2) << std::endl;
-	}
-	catch (CError e)
-	{
-		std::cerr << e;
+		try
+		{
+			tokenize1(str, tok1);
+			tokenize2(tok1, tok2);
+
+			std::cout << untokenize(tok2) << std::endl;
+		}
+		catch (CError e)
+		{
+			std::cerr << e;
+		}
 	}
 
 	return 0;
