27133fc15098db9fa09cb5be63b18996fd983a2b
hotkey <dev@hotkeysoft.net>
2001-08-28 01:08:18 +0000

    Addint exception handling

--- CPU8085/basic/prototypes/tokenize/main.cpp
+++ CPU8085/basic/prototypes/tokenize/main.cpp
@@ -6,6 +6,8 @@
 #include "tokenize.h"
 #include "untokenize.h"
 
+#include "..\include\error.h"
+
 int main(int argc, char* argv[])
 {
 	char str[256];
@@ -14,13 +16,19 @@ int main(int argc, char* argv[])
 
 	strcpy(str, "10 a = -123.45E+2: let variable$ = \"blah\" + chr$(147): i = sqr ( abs ( -2.5 * B - -Z) )");
 
-	tokenize1(str, tok1);
-	tokenize2(tok1, tok2);
+	try
+	{
+		tokenize1(str, tok1);
+		tokenize2(tok1, tok2);
 
-	std::cout << str << std::endl << std::endl;
-	std::cout << untokenize(tok1) << std::endl << std::endl;
-	std::cout << untokenize(tok2) << std::endl;
-	
+		std::cout << str << std::endl << std::endl;
+		std::cout << untokenize(tok1) << std::endl << std::endl;
+		std::cout << untokenize(tok2) << std::endl;
+	}
+	catch (CError e)
+	{
+		std::cerr << e;
+	}
 
 	return 0;
 }
