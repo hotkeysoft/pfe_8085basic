61e7d004ea0d3053e1881aa2b762badaf81204d4
hotkey <dev@hotkeysoft.net>
2001-08-30 02:53:11 +0000

    First draft of expression evaluator

--- CPU8085/basic/prototypes/expreval/expreval.cpp
+++ CPU8085/basic/prototypes/expreval/expreval.cpp
@@ -1,36 +1,208 @@
-// expreval.cpp : Defines the entry point for the console application.
+// expreval.cpp
 //
 
 #include "stdafx.h"
+#include "expreval.h"
 
-#include "..\include\common.h"
+BYTE *currOut;
+BYTE *currIn;
 
-#include <string>
-#include <iostream>
+void L1();
+void L2();
+void L3();
+void L4();
+void L5();
+void L6();
+void L7();
 
-
-BYTE Memory[65536];
-								
-int main(int argc, char* argv[])
+void L0()
 {
-	std::string varName;
+	L1();
 
-	BYTE test[2] = {128 + 11, 0};
-
-	while(1)
+	while (1)
 	{
-		std::cout << "Var name: ";
-		std::cin >> varName;
-		if (varName == "")
+		BYTE currToken = *currIn;
+		if (currToken == K_AND || currToken == K_OR || currToken == K_XOR)
+		{
+			++currIn;
+			L1();
+			*currOut = currToken;
+			++currOut;
+		}
+		else
+		{
 			break;
-
-		Name2Tag(varName, test);
-		Tag2Name(test, varName);
-
-		std::cout << "Got back " << varName << std::endl;
+		}
 	}
-
-
-	return 0;
 }
 
+void L1()
+{
+	if (*currIn == K_NOT)
+	{
+		++currIn;
+		L2();
+		*currOut = K_NOT;
+		++currOut;
+	}
+	else
+	{
+		L2();
+	}
+}
+
+void L2()
+{
+	L3();
+
+	while (1)
+	{
+		BYTE currToken = *currIn;
+		if (currToken == K_NOTEQUAL || 
+			currToken == K_LESSEQUAL || 
+			currToken == K_GREATEREQUAL || 
+			currToken == K_LESS || 
+			currToken == K_GREATER ||
+			currToken == K_EQUAL)
+		{
+			++currIn;
+			L3();
+			*currOut = currToken;
+			++currOut;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+void L3()
+{
+	L4();
+
+	while (1)
+	{
+		BYTE currToken = *currIn;
+		if (currToken == K_ADD || currToken == K_SUBSTRACT)
+		{
+			++currIn;
+			L4();
+			*currOut = currToken;
+			++currOut;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+void L4()
+{
+	L5();
+
+	while (1)
+	{
+		BYTE currToken = *currIn;
+		if (currToken == K_MULTIPLY || currToken == K_DIVIDE)
+		{
+			++currIn;
+			L5();
+			*currOut = currToken;
+			++currOut;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+void L5()
+{
+	if (*currIn == K_NEGATE)
+	{
+		++currIn;
+		L6();
+		*currOut = K_NEGATE;
+		++currOut;
+	}
+	else
+	{
+		L6();
+	}
+}
+
+void L6()
+{
+	L7();
+	while (1)
+	{
+		BYTE currToken = *currIn;
+		if (currToken == K_POWER)
+		{
+			++currIn;
+			L7();
+			*currOut = currToken;
+			++currOut;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+void L7()
+{
+	switch(*currIn)
+	{
+	case SID_CINT:
+		memcpy(currOut, currIn, sizeof(short)+1);
+		currOut += sizeof(short)+1;
+		currIn += sizeof(short)+1;
+		break;
+	case SID_CFLOAT:
+		memcpy(currOut, currIn, sizeof(float)+1);
+		currIn += sizeof(float)+1;
+		currOut += sizeof(float)+1;
+		break;
+	case SID_VAR:
+		memcpy(currOut, currIn, 3);
+		currIn += 3;
+		currOut += 3;
+		break;
+//	case SID_CSTR:
+	default:
+		if (*currIn == '(')
+		{
+			++currIn;
+			L0();
+			++currIn; // Trailing ')'
+		}
+		else if (((*currIn & 0xA0) == 0xA0) || ((*currIn & 0xB0) == 0xB0))
+		{
+			BYTE currToken = *currIn;
+
+			++currIn; // skips keyword
+			++currIn; // skips '('
+			L0();	// expression
+			++currIn; // trailing ')'
+
+			*currOut = currToken;
+			++currOut;
+		}
+		break;
+	}
+}
+
+void expreval(char *in, char *out)
+{
+	currOut = (BYTE *)out;
+	currIn = (BYTE *)in;
+
+	L0();
+
+	*currOut = 0;
+}
