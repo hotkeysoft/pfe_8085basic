84455b56b8bfbae3d4d0b9f8100baeb1521c9266
hotkey <dev@hotkeysoft.net>
2001-08-31 20:11:03 +0000

    First draft of implementation

--- CPU8085/basic/prototypes/strings/strings.cpp
+++ CPU8085/basic/prototypes/strings/strings.cpp
@@ -2,3 +2,53 @@
 
 #include "strings.h"
 
+CStrings::CStrings(void)
+{
+}
+
+CStrings::~CStrings(void)
+{
+}
+
+BYTE *CStrings::Allocate(WORD refBy, BYTE size)
+{
+    LoStrStack -= (size + sizeof(WORD) + sizeof(BYTE));
+	*((WORD *)LoStrStack) = refBy;
+	*(LoStrStack+2) = size;
+	return LoStrStack+3;
+}
+
+void CStrings::Free(BYTE *address)
+{
+	*((WORD *)(address-3)) = 0;	
+}
+
+void CStrings::GarbageCollection()
+{
+
+}
+
+void CStrings::Dump()
+{
+	BYTE *curr = LoStrStack;
+
+	int index = 0;
+
+	std::cerr << "Strings:" << std::endl;
+
+	while (curr < HiStrStack)
+	{
+		WORD refBy = *((WORD *)curr);
+		BYTE size = *(curr+2);
+
+		std::string str; 
+		str.assign(((char *)curr+3), size);
+
+		std::cerr << index << "," << refBy << "," << (int)size << "\t" << str << std::endl;
+
+		curr += size+sizeof(WORD)+sizeof(BYTE);
+		index++;
+	}
+
+}
+
--- CPU8085/basic/prototypes/strings/strings.h
+++ CPU8085/basic/prototypes/strings/strings.h
@@ -8,6 +8,15 @@ public:
 	CStrings(void);
 	~CStrings(void);
 
+	static BYTE *Allocate(WORD refBy, BYTE size);
+	static void Free(BYTE *address);
+
+	static void GarbageCollection();
+
+	static void Clear() {LoStrStack = HiStrStack;};
+
+	static void Dump();
+
 protected:
 
 };
