40c52d6c467a3356e4607df5d56e775ca030ff78
hotkey <dev@hotkeysoft.net>
2001-08-31 03:31:57 +0000

    Tests Get and Set methods

--- CPU8085/basic/prototypes/variables/main.cpp
+++ CPU8085/basic/prototypes/variables/main.cpp
@@ -3,8 +3,75 @@
 
 #include "stdafx.h"
 
+//#include "..\tokenize\tokenize.h"
+//#include "..\tokenize\untokenize.h"
+//#include "expreval.h"
+//#include "exprstack.h"
+
+#include "variables.h"
+
+#include <iostream>
+#include <string>
+
 int main(int argc, char* argv[])
 {
+	LoExpStack = Memory+1024;
+	HiExpStack = LoExpStack+80;
+	CurrExpStack = LoExpStack;
+
+	LoStrStack = Memory+30000;
+	HiStrStack = LoStrStack;
+
+	LoAutoVars = Memory+2048;
+	HiAutoVars = Memory+2048;
+
+	tempVar1 = Memory+768;
+	tempVar2 = tempVar1+5;
+	tempVar3 = tempVar2+5;
+
+
+	BYTE tag1[2];
+	BYTE tag2[2];
+	BYTE tag3[2];
+	BYTE tag4[2];
+
+	Name2Tag("fl", tag1);
+	Name2Tag("sh%", tag2);
+	Name2Tag("a", tag3);
+	Name2Tag("b", tag4);
+
+	try
+	{
+		SetFloat(tempVar1, (float)3.141592654);
+		SetInt(tempVar2, 3810);
+
+		CVariables::Set(tag1, tempVar1);
+		CVariables::Set(tag2, tempVar2);
+
+		CVariables::Dump();
+
+		SetFloat(tempVar1, (float)-666);
+		CVariables::Set(tag1, tempVar1);
+
+		CVariables::Dump();
+
+		SetInt(tempVar2, 0);
+		CVariables::Set(tag2, tempVar2);
+
+		CVariables::Dump();
+
+		BYTE *addr;
+
+		addr = CVariables::Get(tag1);
+		addr = CVariables::Get(tag2);
+		addr = CVariables::Get(tag3);
+	//	addr = CVariables::Get(tag4);
+	}
+	catch (CError e)
+	{
+		std::cerr << e;
+	}
+
 	return 0;
 }
 
