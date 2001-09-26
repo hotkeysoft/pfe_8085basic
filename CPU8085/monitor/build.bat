97c176d45d3ef974adfbcae261d05950535d9fc1
hotkey <dev@hotkeysoft.net>
2001-09-26 23:08:05 +0000

    Included fbuffer to project

--- CPU8085/monitor/build.bat
+++ CPU8085/monitor/build.bat
@@ -1,4 +1,16 @@
 @ECHO OFF
-..\bin\AS8085.EXE -lo ..\basic\io\io
+
+cd ..\basic\io
+..\..\bin\AS8085.EXE -lo io
+cd ..\..\monitor
+
+cd ..\basic\fbuffer
+..\..\bin\AS8085.EXE -lo fbuffer
+cd ..\..\monitor
+
+cd ..\basic\integer
+..\..\bin\AS8085.EXE -lo integer
+cd ..\..\monitor
+
 ..\bin\AS8085.EXE -lo monitor
 ..\bin\aslink.exe -f monitor
