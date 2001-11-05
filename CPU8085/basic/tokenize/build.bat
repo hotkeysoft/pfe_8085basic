f9fd0fade41c8a2454ce026b5bb7b99c4303cb7e
hotkey <dev@hotkeysoft.net>
2001-11-05 16:13:50 +0000

    Added common lib

--- CPU8085/basic/tokenize/build.bat
+++ CPU8085/basic/tokenize/build.bat
@@ -1,4 +1,6 @@
 @ECHO OFF
 ..\..\bin\AS8085.EXE -lo tokenize
 ..\..\bin\AS8085.EXE -lo test
+..\..\bin\AS8085.EXE -lo ..\common\common
 ..\..\bin\aslink.exe -f tokenize
+..\..\bin\hexbin tokenize.ihx
