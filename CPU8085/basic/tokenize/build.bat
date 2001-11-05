1da70aa60c1d8f1eb06a7d0da4ee605604890333
hotkey <dev@hotkeysoft.net>
2001-11-05 21:59:22 +0000

    Added integer library (for ATOI)

--- CPU8085/basic/tokenize/build.bat
+++ CPU8085/basic/tokenize/build.bat
@@ -2,5 +2,6 @@
 ..\..\bin\AS8085.EXE -lo tokenize
 ..\..\bin\AS8085.EXE -lo test
 ..\..\bin\AS8085.EXE -lo ..\common\common
+..\..\bin\AS8085.EXE -lo ..\integer\integer
 ..\..\bin\aslink.exe -f tokenize
 ..\..\bin\hexbin tokenize.ihx
