55be7e7d38e4feb3d1b57ed9ea4e7a4c247a7918
hotkey <dev@hotkeysoft.net>
2001-11-06 14:05:14 +0000

    Added tokenize lib

--- CPU8085/basic/expreval/build.bat
+++ CPU8085/basic/expreval/build.bat
@@ -3,5 +3,6 @@
 ..\..\bin\AS8085.EXE -lo test
 ..\..\bin\AS8085.EXE -lo ..\common\common
 ..\..\bin\AS8085.EXE -lo ..\integer\integer
+..\..\bin\AS8085.EXE -lo ..\tokenize\tokenize
 ..\..\bin\aslink.exe -f expreval
 ..\..\bin\hexbin expreval.ihx
