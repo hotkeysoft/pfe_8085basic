c7b0b22020b3d97512d5d4705a6bec871de9bfb4
hotkey <dev@hotkeysoft.net>
2001-11-11 16:45:30 +0000

    begin implementation of allocate + free

--- CPU8085/basic/strings/strings.asm
+++ CPU8085/basic/strings/strings.asm
@@ -12,9 +12,67 @@ RST7:
 .area	_CODE
 
 ;*********************************************************
-;* STR_XXX:  	
-STR_XXX::
+;* STR_ALLOCATE:	ALLOCATES SPACE FOR A STRING
+;*			IN: LENGTH IN ACC
+;*			    'PARENT' PTR IN 'BC'
+;*			RETURNS ADDRESS IN (H-L)
+STR_ALLOCATE::
+	; KEEP SIZE
+	PUSH	PSW
+	
+	ADI	3				; ADD 3 TO LENGTH
+							; (STRLEN + REF PTR)
+	; CALCULATE -ACC
+	CMA					; ACC = ~ACC
+	INR	A				; ACC = ~ACC+1
+	
+	LHLD	STR_LOPTR			; LOAD LO STR PTR
+	MVI	D,0xFF	
+	MOV	E,A				; -LEN IN DE
+	
+	DAD	D				; LO STR PTR -= LEN
+	
+	SHLD	STR_LOPTR			; SAVE LO STR PTR
+	
+	MOV	M,C				; LO BYTE 'PARENT'
+	INX	H
+	
+	MOV	M,B				; HI BYTE 'PARENT'
+	INX	H
+	
+	POP	PSW				; GET BACK LENGTH
+	
+	MOV	M,A				; STORE LENGTH
+	INX	H
+	
 	RET
+	
+;*********************************************************
+;* STR_FREE:	FREE STR AT (H-L) (SETS 'PARENT' TO 0)
+STR_FREE::
+	PUSH	H				; KEEP ADDRESS
+	
+	DCX	H
+	DCX	H
+	MVI	M,0
+	DCX	H
+	MVI	M,0
+	
+	POP	H				; RESTORE ADDRESS
+	RET
+	
+;*********************************************************
+;* STR_COPY:	COPY STRING (NULL TERMINATED)
+;*		SOURCE:	DE
+;*		DEST: 	HL
+STR_COPY::
+	RET
+
+;*********************************************************
+;* STR_GARBAGECOLLECTION:
+STR_GARBAGECOLLECTION::
+	RET
+
 
 ;*********************************************************
 ;* RAM VARIABLES
@@ -22,4 +80,5 @@ STR_XXX::
 
 .area	DATA	(REL,CON)
 
-;STR_XXX::		.ds	4		; 
+STR_LOPTR::		.ds	2		; BOTTOM OF STR MEMORY
+STR_HIPTR::		.ds	2		; TOP OF STR MEMORY
