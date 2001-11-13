346cfc8a4494f157cc2238099ce0066b90552c1b
hotkey <dev@hotkeysoft.net>
2001-11-13 16:49:24 +0000

    Added string lib

--- CPU8085/basic/expreval/build.bat
+++ CPU8085/basic/expreval/build.bat
@@ -6,5 +6,6 @@
 ..\..\bin\AS8085.EXE -lo ..\integer\integer
 ..\..\bin\AS8085.EXE -lo ..\tokenize\tokenize
 ..\..\bin\AS8085.EXE -lo ..\variables\variable
+..\..\bin\AS8085.EXE -lo ..\strings\strings
 ..\..\bin\aslink.exe -f expreval
 ..\..\bin\hexbin expreval.ihx
