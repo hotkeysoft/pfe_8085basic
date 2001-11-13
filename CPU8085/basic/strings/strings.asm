82bd8dbb7f182de7d5debe2a87c7a1596de7fb45
hotkey <dev@hotkeysoft.net>
2001-11-13 20:18:45 +0000

    Implementation of STR_CMP + tests

--- CPU8085/basic/strings/strings.asm
+++ CPU8085/basic/strings/strings.asm
@@ -82,6 +82,65 @@ STR_COPY::
 	RET
 
 ;*********************************************************
+;* STR_CMP:	COMPARE STRINGS
+;*		STR1:	DE, LEN: B
+;*		STR2: 	HL, LEN: C
+;* RESULTS:  ACC = 0x00 -> SAME
+;*	     ACC = 0x01 -> STR1 > STR2
+;*	     ACC = 0xFF -> STR1 < STR2
+;*
+STR_CMP::
+
+LOOP:	
+	; CHECK FOR END OF STRING
+	MOV	A,B				; LEN1 IN ACC
+	ORA	A				; CHECK FOR 0
+	JZ	END
+
+	MOV	A,C				; LEN2 IN ACC
+	ORA	A				; CHECK FOR 0
+	JZ	END
+	
+	; COMPARE TWO CHARS
+	LDAX	D				; CURR STR1 CHAR IN ACC
+	CMP	M				; COMPARE WITH STR2 CHAR
+	
+	JZ	SKIP
+	JB	LESS
+	JMP	MORE
+	
+SKIP:	; THE TWO CHARS ARE EQUAL
+	INX	D				; STR1PTR++
+	INX	H				; STR2PTR++
+	DCR	B				; LENGTH1--
+	DCR	C				; LENGTH2--
+	JMP	LOOP
+	
+	
+END:	; END OF AT LEAST ONE STRING
+	MOV	A,B				; LEN1 IN ACC
+	ORA	A				; CHECK FOR ZERO
+	JNZ	MORE	
+	
+	MOV	A,C				; LEN2 IN ACC
+	ORA	A				; CHECK FOR ZERO
+	JNZ	LESS
+	
+
+EQUAL:	; EQUAL, RETURN 0
+	MVI	A,0
+	RET
+
+LESS:	; LESS, RETURN -1
+	MVI	A,0xFF
+	RET
+
+MORE:	; MORE, RETURN +1
+	MVI	A,0x01
+	RET
+
+
+;*********************************************************
 ;* STR_GARBAGECOLLECTION:
 STR_GARBAGECOLLECTION::
 	RET
