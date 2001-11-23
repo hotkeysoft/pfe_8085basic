88def146bc74ad46e64a97df5ae2a451b5baee69
hotkey <dev@hotkeysoft.net>
2001-11-23 20:46:50 +0000

    Added error codes, messages + tests

--- CPU8085/basic/error/build.bat
+++ CPU8085/basic/error/build.bat
@@ -1,5 +1,8 @@
 @ECHO OFF
 ..\..\bin\AS8085.EXE -lo error
 ..\..\bin\AS8085.EXE -lo test
+..\..\bin\AS8085.EXE -lo ..\common\common
+..\..\bin\AS8085.EXE -lo ..\io\io
+..\..\bin\AS8085.EXE -lo ..\integer\integer
 ..\..\bin\aslink.exe -f error
 ..\..\bin\hexbin error.ihx
