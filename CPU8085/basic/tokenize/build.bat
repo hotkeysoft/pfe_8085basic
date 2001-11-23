3d27bc791dad00ad12f0526979c12163819818a4
hotkey <dev@hotkeysoft.net>
2001-11-23 20:58:18 +0000

    Added io + error to project

--- CPU8085/basic/tokenize/build.bat
+++ CPU8085/basic/tokenize/build.bat
@@ -3,5 +3,7 @@
 ..\..\bin\AS8085.EXE -lo test
 ..\..\bin\AS8085.EXE -lo ..\common\common
 ..\..\bin\AS8085.EXE -lo ..\integer\integer
+..\..\bin\AS8085.EXE -lo ..\io\io
+..\..\bin\AS8085.EXE -lo ..\error\error
 ..\..\bin\aslink.exe -f tokenize
 ..\..\bin\hexbin tokenize.ihx
