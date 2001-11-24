89cd61de84290231ad6c6e9da13e510fdd8f5ac2
hotkey <dev@hotkeysoft.net>
2001-11-24 15:36:24 +0000

    Added error+io module to project

--- CPU8085/basic/integer/build.bat
+++ CPU8085/basic/integer/build.bat
@@ -2,5 +2,7 @@
 ..\..\bin\AS8085.EXE -lo integer
 ..\..\bin\AS8085.EXE -lo test
 ..\..\bin\AS8085.EXE -lo ..\common\common
+..\..\bin\AS8085.EXE -lo ..\error\error
+..\..\bin\AS8085.EXE -lo ..\io\io
 ..\..\bin\aslink.exe -f integer
 ..\..\bin\hexbin integer.ihx
