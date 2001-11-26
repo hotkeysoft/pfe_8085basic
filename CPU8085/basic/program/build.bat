37ddd18aa7ee04d47ac610d070af4f1dd872b2e9
hotkey <dev@hotkeysoft.net>
2001-11-26 18:51:16 +0000

    Added libs to project

--- CPU8085/basic/program/build.bat
+++ CPU8085/basic/program/build.bat
@@ -7,5 +7,8 @@
 ..\..\bin\AS8085.EXE -lo ..\integer\integer
 ..\..\bin\AS8085.EXE -lo ..\variables\variable
 ..\..\bin\AS8085.EXE -lo ..\tokenize\tokenize
+..\..\bin\AS8085.EXE -lo ..\expreval\expreval
+..\..\bin\AS8085.EXE -lo ..\expreval\evaluate
+..\..\bin\AS8085.EXE -lo ..\strings\strings
 ..\..\bin\aslink.exe -f program
 ..\..\bin\hexbin program.ihx
