25b3122c0dd6c8f770c09b9b31f2c0e9639cdf70
hotkey <dev@hotkeysoft.net>
2001-11-26 18:30:13 +0000

    Added many libs to project

--- CPU8085/basic/error/build.bat
+++ CPU8085/basic/error/build.bat
@@ -4,5 +4,11 @@
 ..\..\bin\AS8085.EXE -lo ..\common\common
 ..\..\bin\AS8085.EXE -lo ..\io\io
 ..\..\bin\AS8085.EXE -lo ..\integer\integer
+..\..\bin\AS8085.EXE -lo ..\program\program
+..\..\bin\AS8085.EXE -lo ..\expreval\expreval
+..\..\bin\AS8085.EXE -lo ..\expreval\evaluate
+..\..\bin\AS8085.EXE -lo ..\variables\variable
+..\..\bin\AS8085.EXE -lo ..\tokenize\tokenize
+..\..\bin\AS8085.EXE -lo ..\strings\strings
 ..\..\bin\aslink.exe -f error
 ..\..\bin\hexbin error.ihx
