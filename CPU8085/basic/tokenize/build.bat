9e2eede0cf3eed1df8488a59df547f4d2a3aa994
hotkey <dev@hotkeysoft.net>
2001-11-27 06:19:16 +0000

    Added libs to project

--- CPU8085/basic/tokenize/build.bat
+++ CPU8085/basic/tokenize/build.bat
@@ -2,8 +2,13 @@
 ..\..\bin\AS8085.EXE -lo tokenize
 ..\..\bin\AS8085.EXE -lo test
 ..\..\bin\AS8085.EXE -lo ..\common\common
+..\..\bin\AS8085.EXE -lo ..\error\error
+..\..\bin\AS8085.EXE -lo ..\expreval\evaluate
+..\..\bin\AS8085.EXE -lo ..\expreval\expreval
 ..\..\bin\AS8085.EXE -lo ..\integer\integer
 ..\..\bin\AS8085.EXE -lo ..\io\io
-..\..\bin\AS8085.EXE -lo ..\error\error
+..\..\bin\AS8085.EXE -lo ..\program\program
+..\..\bin\AS8085.EXE -lo ..\variables\variable
+..\..\bin\AS8085.EXE -lo ..\strings\strings
 ..\..\bin\aslink.exe -f tokenize
 ..\..\bin\hexbin tokenize.ihx
